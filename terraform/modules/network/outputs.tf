13943bee39c6c7a263ca747a741de60961a3ec2a
Robert Castro <oneroberth@gmail.com>
2025-04-23 21:35:36 -0500

    Remove deprecated Kubernetes and Terraform configurations for backend deployment

--- terraform/modules/network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "ID de la VPC creada"
-  value       = google_compute_network.vpc.id
-}
-
-output "private_subnet_id" {
-  description = "ID de la subred privada"
-  value       = google_compute_subnetwork.private.id
-}
